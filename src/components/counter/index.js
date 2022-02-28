d614d94ba4dec7c492e66e04d5152c6c9f7c3b97
Manikandan <manikandan.k@aximsoft.com>
2022-02-28 22:42:58 +0530

    checking changes

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -8,7 +8,7 @@ const Counter = () => {
 
   const imageArr = [
     {
-      id: 1010,
+      id: 1011,
       imgUrl:
         "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
     },
