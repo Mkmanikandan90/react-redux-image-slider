38785a37ede8e6ddc552d22e237c7f7579216222
Manikandan <manikandan.k@aximsoft.com>
2022-03-30 22:26:19 +0530

    test-2 branch commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <div className="ceneter">
           <h2>Test-1</h2>
           <h3> Redux with hook </h3>
+          <h2>Test-2 Checking..</h2>
+          <ul>
+            <li>a. Git checkout from test-1 branch to test-2 branch</li>
+            <li>b. Git fetch</li>
+            <li>c. Git merge origin/test-1 branch</li>
+          </ul>
           <Counter />
         </div>
       </Container>
